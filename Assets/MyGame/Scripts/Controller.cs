698f03156a68a3c1642eaff7196621a026e083a2
verenazmo <verena.zmollnig@htl-salzburg.ac.at>
2021-03-09 09:21:51 +0100

    Add scene switch feature

---
+++ Assets/MyGame/Scripts/Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{
+   public void LoadColorScene()
+    {
+        SceneManager.LoadScene("01ColorScene");
+    }
+
+    public void LoadNumberScene()
+    {
+        SceneManager.LoadScene("02NumberScene");
+    }
+
+    public void LoadWelcomeScene()
+    {
+        SceneManager.LoadScene("00WelcomeScene");
+    }
+}
