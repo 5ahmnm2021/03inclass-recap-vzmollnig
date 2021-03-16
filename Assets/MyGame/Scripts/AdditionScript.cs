de0b437e65a6404fc86ac776f04aa71857a6d5e1
verenazmo <verena.zmollnig@htl-salzburg.ac.at>
2021-03-16 09:14:24 +0100

    updated calculator

--- Assets/MyGame/Scripts/AdditionScript.cs
+++ Assets/MyGame/Scripts/AdditionScript.cs
@@ -13,8 +13,11 @@ public class AdditionScript : MonoBehaviour
     public Color rot = Color.red;
     public Color weiss = Color.white;
 
-    public float if1Float;
-    public float if2Float;
+    float if1Float;
+    float if2Float;
+    
+    bool num1 = true;
+    bool num2 = true;
 
     public void Addition()
     {
@@ -23,25 +26,37 @@ public class AdditionScript : MonoBehaviour
             if1Float = float.Parse(if1.text);
             if1.image.color = weiss;
             message.text = "";
+            num1 = true;
         }
+        
         catch (System.Exception)
         {
-            message.text = "Geben Sie eine gültige Zahl ein";
+            message.text = "Geben Sie eine gültige Zahl ein! ";
             if1.image.color = rot;
+            num1 = false;
         }
 
         try
         {
             if2Float = float.Parse(if2.text);
             if2.image.color = weiss;
+            message.text = "";
+            num2 = true;
         }
+        
         catch (System.Exception)
         {
             message.text = "Geben Sie eine gültige Zahl ein";
             if2.image.color = rot;
+            num2 = false;
         }
 
+    if(num1 == true && num2 == true){
         sum.text = (if1Float + if2Float).ToString();
-        Debug.Log("Die Addition ergibt: " + sum.text);
+        Debug.Log("Das Ergebnis ist " + sum.text);
+      }  
+        else {
+        sum.text = "/";
+    
     }
-}
+}}
